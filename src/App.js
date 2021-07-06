b2d777472e10b19e0da43d83fb9af5894e7a80bd
Nana Kwesi <Emmanuelmensah735@gmail.com>
2021-07-06 14:34:51 +0000

    add src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import User from './Components/User';
+import UserForm from './Components/UserForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        }
+    }
+
+    handleAddUser = (newUser) => {
+        this.setState({ 
+            users: [newUser, ...this.state.users] 
+        })
+    }
+
+    render() {
+        const users = this.state.users.map((user, index) => {
+            return (
+               <User user={user} index={index}/>
+            )
+        })
+
+        return (
+            <div style={{ margin: "50px" }}>
+                <UserForm addUser={this.handleAddUser} />
+                {users}
+            </div>
+        );
+    }
 }
 
 export default App;
