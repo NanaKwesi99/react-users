1aff3c304c409729ca12ac4cc1ebfa635ed3d2eb
Weggins <Emmanuelmensah735@gmail.com>
2021-08-24 13:39:54 +0000

    updated Actions

--- src/Actions/UserActions.js
+++ src/Actions/UserActions.js
@@ -1,9 +1,23 @@
 export const ADD_USER ="ADD_USER";
+export const DELETE_USER ="DELETE_USER";
+export const EDIT_USER ="EDIT_USER";
 
 export const addUser = (user) => {
     user.id= Math.random().toString();
     return{
         type: ADD_USER,
         payload: user
+    }
+}     
+export const deleteUser = (userId) => {              
+    return{
+        type: "DELETE_USER",
+        payload: userId
+    }
+}
+export const editUser = (updatedUser) => {              
+    return{
+        type: "EDIT_USER",
+        payload: updatedUser
     }
 }
