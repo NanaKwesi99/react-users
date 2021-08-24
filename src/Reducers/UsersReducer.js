cfd46dd04323a14aec9e4484e5eb2acb8415a3ef
Weggins <Emmanuelmensah735@gmail.com>
2021-08-24 13:40:11 +0000

    updated Reducer

--- src/Reducers/UsersReducer.js
+++ src/Reducers/UsersReducer.js
@@ -1,22 +1,33 @@
-import { ADD_USER } from '../Actions/UserActions';
+import { ADD_USER, DELETE_USER, EDIT_USER } from "../Actions/UserActions";
 
 const initialState = {
-    users: [
-        {
-          name: 'John',
-          email: 'john@gmail.com',
-          age: '20'
-        }
-      ],
-    } 
+  users: [
+    {
+      name: "Manuel",
+      email: "manuel222@email.com",
+      gen: 20,
+      id: "84379843.fsdjhj",
+    },
+    { name: "Kofi", email: "kofi526@email.com", gen: 5, id: "skdlj4839483" },
+  ],
+};
 
-    const UsersReducer = (state=initialState, action)=> { 
-        switch (action.type) { 
-            case ADD_USER: 
-                return  {...state.users,users:[...state.users,action.payload] };
-            default: 
-               return state; 
-        }
-     };
-    
- export default UsersReducer;
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_USER:
+      return { ...state.users, users: [...state.users, action.payload] };
+    case DELETE_USER:
+      const savedUsers = state.users.filter((user) => {
+        return user.id !== action.payload;
+      });
+      return {...state, users: savedUsers};
+    case EDIT_USER:
+      return {...state, users: state.users.map((user) =>
+        user.id === action.payload.id ? action.payload : user
+      )}
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
