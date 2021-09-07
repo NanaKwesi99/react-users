806796a988edfe0874364849f9ad6f47d1e19f39
Weggins <Emmanuelmensah735@gmail.com>
2021-09-07 15:00:05 +0000

    update store.js

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,6 +1,20 @@
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import UsersReducer from "../Reducers/UsersReducer";
+import {getFirebase, reactReduxFirebase} from "react-redux-firebase";
+import { getFirestore, reduxFirestore } from "redux-firestore";
+import thunk from "redux-thunk";
+import firebase from "../firebase/config";
 
-const Store = createStore(UsersReducer) 
+
+
+
+const Store = createStore(UsersReducer, compose (
+     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(firebase),
+    reduxFirestore(firebase)
+    )); 
+
+
+
 
 export default Store;
